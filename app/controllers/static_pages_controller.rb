cc3e2238153ea3bc544f3ce072f1969e6a88eb4e
James Edmondson <jedmondson.ba@gmail.com>
2016-04-22 19:20:53 +0000

    sendgrid

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,6 +7,13 @@ class StaticPagesController < ApplicationController
   end
 
   def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(:from => @email,
+      :to => 'jedmondson.ba@gmail.com',
+      :subject => "A new contact form message from #{@name}",
+      :body => @message).deliver_now
   end
 
 end
