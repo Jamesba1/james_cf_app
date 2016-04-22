225946741f4e4b4e09de69382442547395a2c2ae
James Edmondson <jedmondson.ba@gmail.com>
2016-04-22 13:37:26 +0000

    stuck

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -71,4 +71,16 @@ class ProductsController < ApplicationController
     def product_params
       params.require(:product).permit(:name, :description, :image_url, :colour, :price)
     end
+
+    def index
+      if params[:q]
+        search_term = params[:q]
+        @products = Product.where("name LIKE ?", "%#{search_term}%")
+      else
+        @products = Product.all
+      end
+  
+end
+
+
 end
