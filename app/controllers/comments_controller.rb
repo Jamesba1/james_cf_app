5563eb44c942c10e29826986f6de7ddb1783140e
James Edmondson <jedmondson.ba@gmail.com>
2016-05-02 13:10:33 +0000

    changes

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -11,6 +11,8 @@ def create
         format.html { redirect_to @product, alert: 'Review was not saved successfully.' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
+    end
+  end
 
   def destroy
   	 @comment = Comment.find(params[:id])
@@ -19,11 +21,13 @@ def create
   redirect_to product
   end
 
+   def index
+    @comments = Comment.all.paginate(page: params[:page], per_page: 5)
+  end
+
   private
 
   def comment_params
     params.require(:comment).permit(:user_id, :body, :rating)
   end
 end
-
-
